dc39f4a1c5f2da31fd73405f264e121227486f9c
Cameron Price <pricec@Camerons-MacBook-Pro.local>
2019-04-10 16:58:33 +1200

    Use builtin scope feature to limit unique validation to game only

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -3,6 +3,7 @@ class Guess < ApplicationRecord
 
   validates :letter, presence: true
   validates :letter, length: { is: 1 }
+  validates :letter, uniqueness: { scope: :game }
   validate :validates_game_is_active
   validate :validates_duplicate_guesses
 
@@ -12,11 +13,5 @@ class Guess < ApplicationRecord
     return unless game
 
     errors.add(:game, "has already been completed") unless game.active?
-  end
-
-  def validates_duplicate_guesses
-    return unless game
-
-    errors.add(:letter, "has already been guessed") if game.guessed_letters.include?(letter)
   end
 end
